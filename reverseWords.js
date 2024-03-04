82f575c10dc618ff237aebd9076dfa434e377f67
arsalannury <arsiiiland@yahoo.com>
2024-03-04 17:32:17 +0330

    ⚡️

---
+++ reverseWords.js
@@ -0,0 +1,9 @@
+function reverseWords(str) {
+    console.log(str.split(""));
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
+// console.log(reverseWords("apple"));
+// console.log(reverseWords("a b c d"));
+// console.log(reverseWords("double  spaced  words"));
