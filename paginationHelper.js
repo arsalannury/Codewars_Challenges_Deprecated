f6d848832fc257c72c9a3323538a9324fff491d0
arsalannury <arsiiiland@yahoo.com>
2024-05-12 12:16:08 +0330

    🎨

--- paginationHelper.js
+++ paginationHelper.js
@@ -1,7 +1,7 @@
- class PaginationHelper {
+class PaginationHelper {
   constructor(collection, itemsPerPage) {
     this.collection = collection;
-    this.itemsPerPage = itemsPerPage;
+    this.itemsPerPage = itemsPerPage; // itemPerPage
   }
   itemCount() {
     return this.collection.length;
@@ -10,8 +10,7 @@
     return Math.ceil(this.collection.length / this.itemsPerPage);
   }
   pageItemCount(pageIndex) {
-    // returns the number of items on the current page. page_index is zero based.
-    // this method should return -1 for pageIndex values that are out of range
+   
   }
   pageIndex(itemIndex) {
     // determines what page an item is on. Zero based indexes
